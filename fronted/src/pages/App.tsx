de3c054ee5f061b275e851a6963b88ecd1a43d62
nueve0936 <nain.ayon@uabc.edu.mx>
2026-05-12 08:15:04 -0700

    first commit

---
+++ fronted/src/pages/App.tsx
@@ -0,0 +1,17 @@
+ /* 
+ import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png' 
+*/ 
+
+function App() {
+
+  return (
+    <>
+    <div>HOLA MUNDO</div>
+    </>
+  )
+}
+
+export default App
